478e07e699675acdca14a58d65e345ecc01400d8
DJosueMM <djosuemm@gmail.com>
2023-04-24 21:13:56 -0600

    Immediate Assertions verification

--- SPI_interface_mode_b00/tb/tb_top_module_spi.sv
+++ SPI_interface_mode_b00/tb/tb_top_module_spi.sv
@@ -2,21 +2,21 @@
 
 module tb_top_module_spi;
 
-    logic            clk_pi;
+    //Estimulos de entrada
+    logic clk_pi;
+    logic wr_pi;
+    logic reg_sel_pi; 
+     
+    logic [12 : 0] entrada_pi;  
+    logic [1  : 0] addr_pi;     
 
-    logic            wr_pi;
-    logic            reg_sel_pi;  //sw
-    logic  [12 : 0]  entrada_pi;  //sw
-    logic  [1  : 0]  addr_pi;     //sw
-
-    logic [15 : 0]   leds_po;     //Salida de datos
-    
-    //logic            miso_pi;     //Puerto serial 
+    //Estimulos de salida
+    logic [15 : 0]   leds_po;     
     logic            mosi_po;
     logic            sclk_po;
     logic            ss_po;
 
-
+    //Instancia del modulo top de la interfaz SPI
     top_module_spi DUT (
     .clk_pi     (clk_pi),
     .wr_pi      (wr_pi),
@@ -24,74 +24,147 @@ module tb_top_module_spi;
     .entrada_pi (entrada_pi),
     .addr_pi    (addr_pi),
     .leds_po    (leds_po),
-    .miso_pi    (mosi_po),     
+    .miso_pi    (mosi_po),  //Se conecta el MISO con el MOSI   
     .mosi_po    (mosi_po),   
     .sclk_po    (sclk_po),
     .ss_po      (ss_po)
     );
 
-    initial   clk_pi = 0;
+    //Se genera un estimulo de clk a 100MHz
+    initial clk_pi = 0;
     always #5 clk_pi = ~clk_pi;
 
     initial begin
     
-        //Pasa el Locked PLL
-        #10000
+        //Pasa el Locked del PLL
+        #10000;
         
         //Inicializacion
-        wr_pi       = 0;
-        reg_sel_pi  = 0;
-        entrada_pi  = '0;
-        addr_pi     = 2'b00;
+        wr_pi = 0;
+        reg_sel_pi = 0;
+        entrada_pi = '0;
+        addr_pi = 2'b00;
 
-        //Ingreso dato
-        #10000
-        wr_pi       = 0;
-        reg_sel_pi  = 1;
-        entrada_pi  = 12'b000010100100;
-        addr_pi     = 2'b00;
-
-        //Cargo dato
-        #200
-        wr_pi       = 1;
+        //Ingreso primer dato
+        #10000;
         
-        #100
-        wr_pi       = 0;
-
-        
-        //Ingreso una transaccion
-        #200
-        wr_pi       = 0;
-        reg_sel_pi  = 0;
-        entrada_pi  = 12'b000000010111;
-        addr_pi     = 2'b00;
-        
+        wr_pi = 0;
+        reg_sel_pi = 1;
+        entrada_pi = 12'b000010100100;
+        addr_pi = 2'b00;
         
         //Cargo dato
-        #200
-        wr_pi       = 1;
+        #110;
+        wr_pi = 1;
+        #110;
+        wr_pi = 0;
         
-        #100
-        wr_pi       = 0;
-        reg_sel_pi  = 1;
+        #110;
         
-        //Ingreso una transaccion
-        #200
-        wr_pi       = 0;
-        reg_sel_pi  = 0;
-        entrada_pi  = 12'b000000011011;
-        addr_pi     = 2'b00;
-        
+        wr_pi = 0;
+        reg_sel_pi = 1;
+        entrada_pi = 12'b000010100100;
+        addr_pi = 2'b11;
+
+        //Cargo dato
+        #110;
+        wr_pi = 1;
+        #110;
+        wr_pi = 0;
+
+        //Ingreso primer transaccion
+        #200;
+        wr_pi = 0;
+        reg_sel_pi = 0;
+        entrada_pi = 12'b000000000011; //Una transaccion dato de registro
+        addr_pi = 2'b00;
         
         //Cargo dato
-        #300_000
-        wr_pi       = 1;
+        #200;
+        wr_pi = 1;
+        #100;
+        wr_pi = 0;
+
+        #135_000;
         
-        #100
-        wr_pi       = 0;
-        reg_sel_pi  = 1;
+        //Verifico primer transaccion
+        assert (leds_po[0] == 1'b0)
+        else $error("%m no se actualiza send en registro de control");
         
+        assert (leds_po[15 : 13] == 3'b001)
+        else $error("%m no se muestra la cantidad de transacciones realizadas");
+        
+        assert (ss_po  == 1'b1)
+        else $error("%m no se deshabilita el esclavo al terminar");
+        
+        assert (sclk_po == 1'b0)
+        else $error("%m no se tiene la polaridad del sclk en reposo de 0");
+        
+        #100;
+        reg_sel_pi = 1;
+        
+        #100;
+        assert (leds_po[7 : 0] == 8'b10100100)
+        else $error("%m no se recibio el dato esperado");
+        
+        
+         //Ingreso segunda transaccion
+        #200;
+        wr_pi = 0;
+        reg_sel_pi = 0;
+        entrada_pi = 12'b000000101011; //tres transacciones todo en 0
+        addr_pi = 2'b00;
+        
+        #200;
+        //Cargo dato
+        wr_pi = 1;
+        
+        #100;
+        wr_pi = 0;
+        
+        #400_000;
+        
+        //Verifico segunda transaccion
+        assert (leds_po[0] == 1'b0)
+        else $error("%m no se actualiza send en registro de control");
+        
+        assert (leds_po[15 : 13] == 3'b011)
+        else $error("%m no se muestra la cantidad de transacciones realizadas");
+        
+        assert (ss_po  == 1'b1)
+        else $error("%m no se deshabilita el esclavo al terminar");
+        
+        assert (sclk_po == 1'b0)
+        else $error("%m no se tiene la polaridad del sclk en reposo de 0");
+        
+        #100;
+        reg_sel_pi = 1;
+        
+        #100;
+        assert (leds_po[7 : 0] == 8'b00000000)
+        else $error("%m no se recibio el dato esperado en la primer transaccion");
+        
+        #100;
+        addr_pi = 2'b01;
+        
+        #100;
+        assert (leds_po[7 : 0] == 8'b00000000)
+        else $error("%m no se recibio el dato esperado en la segunda transaccion");
+        
+        #100;
+        addr_pi = 2'b10;
+        
+        #100;
+        assert (leds_po[7 : 0] == 8'b00000000)
+        else $error("%m no se recibio el dato esperado en la tercer transaccion");
+        
+        #100;
+        addr_pi = 2'b11;
+        
+        #100;
+        assert (leds_po[7 : 0] == 8'b10100100)
+        else $error("%m se cambio un registro que no debia participar en una transaccion");
+        
+        $finish;
     end
-
-
 endmodule
